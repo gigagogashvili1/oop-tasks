d52ca97328e16e34eace17fad79feede7d7bc157
gigagogashvili <giga.gogashvili@makingscience.com>
2023-06-29 15:18:53 +0400

    uncommented things

--- src/relations/one-to-one/index.ts
+++ src/relations/one-to-one/index.ts
@@ -1,19 +1,19 @@
-// class Game {
-//     constructor(public gameTitle: string, public engine: Enginee) {}
+class Game {
+    constructor(public gameTitle: string, public engine: Enginee) {}
 
-//     public printEngineInformation() {
-//         console.log(`${this.gameTitle} is using ${this.engine.engineTitle}`);
-//     }
-// }
-// class Enginee {
-//     public game?: Game;
-//     constructor(public engineTitle: string) {}
+    public printEngineInformation() {
+        console.log(`${this.gameTitle} is using ${this.engine.engineTitle}`);
+    }
+}
+class Enginee {
+    public game?: Game;
+    constructor(public engineTitle: string) {}
 
-//     public createNewGame(gameTitle: string) {
-//         this.game = new Game(gameTitle, this);
-//     }
-// }
+    public createNewGame(gameTitle: string) {
+        this.game = new Game(gameTitle, this);
+    }
+}
 
-// const unrealEngine = new Enginee('Unreal Engine');
-// unrealEngine.createNewGame('fortnite');
-// unrealEngine.game?.printEngineInformation();
+const unrealEngine = new Enginee('Unreal Engine');
+unrealEngine.createNewGame('fortnite');
+unrealEngine.game?.printEngineInformation();
